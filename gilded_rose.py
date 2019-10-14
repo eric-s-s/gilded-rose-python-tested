5a0b66703e22a252a14a0c76dc89a6a55cdbc7fa
Eric Shaw <eric.shaw@datarobot.com>
2019-10-13 21:41:57 -0400

    change 1= to not ==

--- gilded_rose.py
+++ gilded_rose.py
@@ -9,10 +9,10 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if (item.name != "Aged Brie" and
-                    item.name != "Backstage passes to a TAFKAL80ETC concert"):
+            if (not item.name == "Aged Brie" and
+                    not item.name == "Backstage passes to a TAFKAL80ETC concert"):
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if not item.name == "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
@@ -24,13 +24,13 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if not item.name == "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if not item.name == "Aged Brie":
+                    if not item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if not item.name == "Sulfuras, Hand of Ragnaros":
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
