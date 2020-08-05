40b4fc127c82273d325b64fe55d5edea8223106b
Eric Shaw <eric.shaw@datarobot.com>
2020-08-04 22:34:55 -0400

    updated tcr

--- tcr.sh
+++ tcr.sh
@@ -3,7 +3,7 @@
 if pytest ; then
     black .
     git add .
-    git commit -m "$(echo $@)"
+    git commit -m "$(echo $@)"  # read the arguments as the commit message
 fi
 git reset --hard
 git status
